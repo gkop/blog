6e97dac850ee6e7e121250e0a77cb7c8ecde9883
Gabe Kopley <gabe@coshx.com>
2013-02-19 19:27:44 -0800

    Follow getting started guide up to / including posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Blog::Application.routes.draw do
+  resources :posts
+
+
+  get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
