d41aaa50857d8b3217229bc26c08169d72ce1cbe
Gabe Kopley <gabe@coshx.com>
2013-09-17 20:31:05 -0700

    Starting to capify

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "blog"
+set :repository,  "https://github.com/gkop/blog"
+set :host, "ec2-54-215-222-193.us-west-1.compute.amazonaws.com"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, host
+role :app, host
+role :db,  host, :primary => true # This is where Rails migrations will run
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
